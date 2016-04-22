75c68499a86bae957f60bf6be2dcbcecc6313fbd
perfecto25 <mike.reider@gmail.com>
2016-04-21 22:15:50 -0400

    new

--- manifests/windows_reboot.pp
+++ manifests/windows_reboot.pp
@@ -1,9 +1,19 @@
 class various::windows_reboot {
 
 include chocolatey
+include reboot
 
-package { '7zip':
-  ensure => absent,
+$packages = [ '7zip', 'notepadplusplus', 'cmder' ]
+
+
+# clear pending reboots on target
+reboot { 'before':
+    when => pending,
+}
+
+
+package { $packages:
+  ensure => installed,
   provider => chocolatey,
 }
 
